c101aa6c14aa5251c7c2271685e8f78e8a34b757
rogueking <miguel@muniz.org>
2025-01-30 15:29:28 -0800

    Added binaries for IR

---
+++ IR/install.sh
@@ -0,0 +1,26 @@
+!#/bin/bash
+
+echo "what os is this?"
+echo "1. linux-gnu"
+echo "2. linux-musl"
+
+read -p "enter: " response
+
+case "$response" in
+    [1]* ) echo "linux-gnu selected";;
+    [2]* ) echo "linux-musl selected";;
+    * ) echo "Invalid input. Please enter 1 or 2"; exit 1;;
+esac
+
+if [ "$response" == "1" ]; then
+    echo "Moving binaries from linux-gnu"
+    mv -f linux-gnu/* /usr/bin
+elif [ "$response" == "2" ]; then
+    echo "Moving binaries from linux-musl"
+    mv -f linux-musl/* /usr/bin
+else
+    echo "Invalid input. Please enter 1 or 2."
+    exit 1
+fi
+
+echo "Binaires have been moved successfully!"
