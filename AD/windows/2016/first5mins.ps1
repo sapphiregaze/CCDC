21c94943d9809b9da31cea0069e5570a4f109518
soggycats <brandonramirez@unr.edu>
2025-02-01 14:34:05 -0800

    changes to first 5 minutes script

--- AD/windows/2016/first5mins.ps1
+++ AD/windows/2016/first5mins.ps1
@@ -1,3 +1,67 @@
+Import-Module -Name ActiveDirectory
+Import-Module -Name GroupPolicy
+
+#placing this first because UCI
+# Disable SMBv1
+Write-Output "Disabling SMBv1"
+try {
+    Set-SmbServerConfiguration -EnableSMB1Protocol $false
+}
+catch {
+    Write-Output "Failed to disable SMBv1! This is a critical security vulnerability!"
+}
+
+# enable Kerberos Pre-Authentication for all users
+Write-Output "Enabling Kerberos Pre-Authentication for all users"
+try {
+    Set-ADDefaultDomainPasswordPolicy -PreAuthNotRequired $false
+    Get-ADUser -Filter {DoesNotRequirePreAuth -eq $true} | Set-ADUser -DoesNotRequirePreAuth $false -Confirm:$false
+}
+catch {
+    Write-Output "Failed to enable Kerberos Pre-Authentication for all users!"
+}
+
+# Disable Spooler Service
+Write-Output "Disabling Print Spooler service"
+try {
+    Stop-Service -Name "Spooler" -ErrorAction Stop
+    Set-Service -Name "Spooler" -StartupType Disabled
+}
+catch {
+    Write-Host "Failed to disable Print Spooler service!"
+}
+
+# Mitigate ZeroLogon
+Write-Output "Mitigating ZeroLogon"
+
+Write-Output "Enabling enforcement of netlogon secure channel"
+try {
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -Name "FullSecureChannelProtection" -Value 1 -Type DWord
+}
+catch {
+    Write-Output "Failed to enable enforcement of netlogon secure channel!"
+}
+
+Write-Output "Removing vulnerable channel allow list"
+$vulAllows = Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\VulnerableChannelAllowList"
+if ($vulAllows) {
+    Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters\VulnerableChannelAllowList" -Name "VulnerableChannelAllowList" -Force
+    Write-Output "Removed vulnerable channel allow list"
+} else {
+    Write-Output "Vulnerable channel allow list does not exist"
+}
+
+# Mitigate noPac
+Write-Output "Mitigating noPac"
+try {
+    # dissallow users from setting their own machine account password
+    # Set-ADDomain -Identity $env:USERDNSDOMAIN -replace @{"ms-DS-MachineAccountQuota"="0"}
+    Set-ADDomain (Get-ADDomain).distinguishedname -Replace @{"ms-ds-MachineAccountQuota"="0"}
+} catch {
+    Write-Output "Failed to mitigate noPac!"
+}
+
+
 # Ensure NuGet is installed
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 [Net.ServicePointManager]::SecurityProtocol
@@ -13,26 +77,32 @@ Register-PSRepository -Default -InstallationPolicy Trusted
 
 #Install and apply modules
 Install-Module -Name PSWindowsUpdate -Force -Confirm:$false
-Install-Module -Name ActiveDirectory -Force -Confirm:$false
-Install-Module -Name GroupPolicy -Force -Confirm:$false
 
 Import-Module -Name PSWindowsUpdate
-Import-Module -Name ActiveDirectory
-Import-Module -Name GroupPolicy
+
+Write-Output "Making C:\tmp directory and C:\tmp\gpos directory"
+#Make C:\tmp directory
+New-Item -Path "C:\tmp" -ItemType Directory
+
+#Make C:\tmp\gpos directory
+New-Item -Path "C:\tmp\gpos" -ItemType Directory
 
 # Change Administrator password
 $admin = Get-LocalUser -Name Administrator
-$password = Read-Host -AsSecureString "Enter new password: "
+$password = Read-Host -AsSecureString "Enter new administrator password: "
+$confirmPassword = Read-Host -AsSecureString "Confirm new administrator password: "
+if ($password -ne $confirmPassword) {
+    Write-Error "Passwords do not match."
+    return
+}
 Set-LocalUser -Name Administrator -Password $password
 
-#Disable Guest Account
-Set-ADUser -Identity "Guest" -Enabled $false
-Set-LocalUser -Name Guest -Enabled $false
-
-# Get Default Naming Context
-$defaultNamingContext = (Get-ADRootDSE).defaultNamingContext
-
 $anotherPassword = Read-Host -AsSecureString "Enter new password for new admin: "
+$confirmPassword = Read-Host -AsSecureString "Confirm new password for new admin: "
+if ($anotherPassword -ne $confirmPassword) {
+    Write-Error "Passwords do not match."
+    return
+}
 
 $usersOU = "OU=Users,$defaultNamingContext"
 
@@ -40,15 +110,23 @@ $usersOU = "OU=Users,$defaultNamingContext"
 New-ADUSer  -Name "Inconspicious User" `
             -SamAccountName "iuser" `
             -UserPrincipalName "iuser@$env:USERDNSDOMAIN" `
-            -Path $usersOU `
             -AccountPassword $anotherPassword `
             -Enabled $true
+
+#Disable Guest Account
+Write-Output "Disabling Guest Account"
+Set-ADUser -Identity "Guest" -Enabled $false
+Set-LocalUser -Name Guest -Enabled $false
+
+# Get Default Naming Context
+$defaultNamingContext = (Get-ADRootDSE).defaultNamingContext
 
 # Add backup Admin to Domain Admins, Enterprise Admins, Schema Admins, Administrators, DNSADmins, and Remote Desktop Users
 Add-ADGroupMember -Identity "Domain Admins" -Members "iuser"
 Add-ADGroupMember -Identity "Enterprise Admins" -Members "iuser"
 Add-ADGroupMember -Identity "Schema Admins" -Members "iuser"
 Add-ADGroupMember -Identity "Administrators" -Members "iuser"
+Add-LocalGroupMember -Group "Administrators" -Member "iuser"
 Add-ADGroupMember -Identity "DNSAdmins" -Members "iuser"
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members "iuser"
 
@@ -57,33 +135,47 @@ $domainAdmins = Get-ADGroupMember -Identity "Domain Admins" | Where-Object { $_.
 $enterpriseAdmins = Get-ADGroupMember -Identity "Enterprise Admins" | Where-Object { $_.SamAccountName -ne "Administrator" -and $_.SamAccountName -ne "iuser" }
 $schemaAdmins = Get-ADGroupMember -Identity "Schema Admins" | Where-Object { $_.SamAccountName -ne "Administrator" -and $_.SamAccountName -ne "iuser" }
 $administrators = Get-ADGroupMember -Identity "Administrators" | Where-Object { $_.SamAccountName -ne "Administrator" -and $_.SamAccountName -ne "iuser" }
+$localAdministrators = Get-LocalGroupMember -Group "Administrators" | Where-Object { $_.Name -ne "Administrator" -and $_.Name -ne "iuser" }
 $dnsAdmins = Get-ADGroupMember -Identity "DNSAdmins" | Where-Object { $_.SamAccountName -ne "Administrator" -and $_.SamAccountName -ne "iuser" }
 $remoteDesktopUsers = Get-ADGroupMember -Identity "Remote Desktop Users" | Where-Object { $_.SamAccountName -ne "Administrator" -and $_.SamAccountName -ne "iuser" }
 
+# Write the removed users to a file
+
+$oldDomainAdminsFile = "C:\tmp\oldDomainAdmins.txt"
 foreach ($user in $domainAdmins) {
     Remove-ADGroupMember -Identity "Domain Admins" -Members $user -Confirm:$false
+    Add-Content -Path $oldDomainAdminsFile -Value $user.SamAccountName
 }
+$oldEnterpriseAdminsFile = "C:\tmp\oldEnterpriseAdmins.txt"
 foreach ($user in $enterpriseAdmins) {
     Remove-ADGroupMember -Identity "Enterprise Admins" -Members $user -Confirm:$false
+    Add-Content -Path $oldEnterpriseAdminsFile -Value $user.SamAccountName
 }
+$oldSchemaAdminsFile = "C:\tmp\oldSchemaAdmins.txt"
 foreach ($user in $schemaAdmins) {
     Remove-ADGroupMember -Identity "Schema Admins" -Members $user -Confirm:$false
+    Add-Content -Path $oldSchemaAdminsFile -Value $user.SamAccountName
 }
+$oldAdministratorsFile = "C:\tmp\oldAdministrators.txt"
 foreach ($user in $administrators) {
     Remove-ADGroupMember -Identity "Administrators" -Members $user -Confirm:$false
+    Add-Content -Path $oldAdministratorsFile -Value $user.SamAccountName
 }
+$oldLocalAdministratorsFile = "C:\tmp\oldLocalAdministrators.txt"
+foreach ($user in $localAdministrators) {
+    Remove-LocalGroupMember -Group "Administrators" -Member $user.Name
+    Add-Content -Path $oldLocalAdministratorsFile -Value $user.Name
+}
+$oldDnsAdminsFile = "C:\tmp\oldDnsAdmins.txt"
 foreach ($user in $dnsAdmins) {
     Remove-ADGroupMember -Identity "DNSAdmins" -Members $user -Confirm:$false
+    Add-Content -Path $oldDnsAdminsFile -Value $user.SamAccountName
 }
+$oldRemoteDesktopUsersFile = "C:\tmp\oldRemoteDesktopUsers.txt"
 foreach ($user in $remoteDesktopUsers) {
     Remove-ADGroupMember -Identity "Remote Desktop Users" -Members $user -Confirm:$false
+    Add-Content -Path $oldRemoteDesktopUsersFile -Value $user.SamAccountName
 }
-
-#Make C:\tmp directory
-New-Item -Path "C:\tmp" -ItemType Directory
-
-#Make C:\tmp\gpos directory
-New-Item -Path "C:\tmp\gpos" -ItemType Directory
 
 #Backup GPOs
 Backup-GPO -All -Path "C:\tmp\gpos"
@@ -98,6 +190,10 @@ Get-GPO -All | Remove-GPO -Confirm:$false
 
 #Unlink default domain policy and default domain controller policy
 Get-GPO -All | Where-Object { $_.DisplayName -eq "Default Domain Policy" -or $_.DisplayName -eq "Default Domain Controllers Policy" } | ForEach-Object { $_ | Remove-GPLink -Target "dc=$env:USERDNSDOMAIN" }
+
+Remove-GPLink -Name "Default Domain Policy" -Target "dc=$env:USERDNSDOMAIN" -LinkEnabled Yes
+
+Remove-GPLink -Name "Default Domain Controllers Policy" -Target "ou=dc=$env:USERDNSDOMAIN" -LinkEnabled Yes
 
 #Remake default domain policy and default domain controller policy
 New-GPO -Name "Default Domain Policy" -Comment "Default Domain Policy" | New-GPLink -Target "dc=$env:USERDNSDOMAIN" -LinkEnabled Yes
@@ -136,9 +232,6 @@ Set-MpPreference -LowThreatDefaultAction 6
 Set-MpPreference -SevereThreatDefaultAction 6
 Set-MpPreference -PUAProtection 1
 
-#Disable PSRemoting
-Disable-PSRemoting
-
 #Get User Accounts that are used as Service Accounts
 Get-ADUser -Filter {ServicePrincipalName -ne "$null"} -Properties ServicePrincipalName
 
@@ -154,11 +247,15 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" -Name "AllowRemoteShellAccess" -Value 0 -Type DWord
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" -Name "AllowAutoConfig" -Value 0 -Type DWord
 
-Stop-Service -Name WinRM
-Set-Service -Name WinRM -StartupType Disabled
+#Disable PSRemoting
+Disable-PSRemoting -Force
 
-# Disable SMBv1
-Set-SmbServerConfiguration -EnableSMB1Protocol $false
+# Disable Windows Remote Management Service
+Stop-Service -Name WindowsRemoteManagement -Force
+Set-Service -Name WindowsRemoteManagement -StartupType Disabled
+
+# Only allow remote access to members of Administrators group
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 0 -Type DWord
 
 # Disable LLMNR
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Name "EnableMulticast" -Value 0 -Type DWord
@@ -175,32 +272,8 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SrpV2" -Name "
 #list shadow copies
 vssadmin list shadows
 
-# Enable SMB Encryption
 Write-Output "Enabling SMB Encryption"
 Set-SmbServerConfiguration –EncryptData $true -Confirm:$false
-
-# Enable SMB Signing
-Write-Output "Enabling SMB Signing"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "RequireSecuritySignature" -Value 1 -Type DWord
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "EnableSecuritySignature" -Value 1 -Type DWord
-
-# Enable NTLMv2
-Write-Output "Enabling NTLMv2"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5 -Type DWord
-
-# Disable LM
-Write-Output "Disabling LM"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LMCompatibilityLevel" -Value 0 -Type DWord
-
-# Temporarily disable sshd
-Write-Output "Temporarily disabling sshd service"
-Stop-Service -Name sshd
-Set-Service -Name sshd -StartupType Disabled
-
-# Disable Spooler Service
-Write-Output "Disabling Spooler Service"
-Stop-Service -Name Spooler
-Set-Service -Name Spooler -StartupType Disabled
 
 #MORE MITIGATIONS, FROM WGU's deepend.ps1 script
 
@@ -237,21 +310,49 @@ foreach ($nic in $nics) {
 }
 #enable powershell logging
 Write-Output "Enabling powershell logging"
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v EnableModuleLogging /t REG_DWORD /d 1 /f
+try {
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v EnableModuleLogging /t REG_DWORD /d 1 /f
+}
+catch {
+    Write-Output "Failed to enable powershell module logging"
+}
 
 # enable script block logging
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
+Write-Output "Enabling powershell script block logging"
+try {
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
+}
+catch {
+    Write-Output "Failed to enable powershell script block logging"
+}
 
 # enable powershell transcription
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v EnableTranscripting /t REG_DWORD /d 1 /f
+Write-Output "Enabling powershell transcription"
+try {
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" /v EnableTranscripting /t REG_DWORD /d 1 /f
+}
+catch {
+    Write-Output "Failed to enable powershell transcription"
+}
 
 # enable powershell constrained language mode
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Audit" /v ProcessCreationIncludeCmdLine_Enabled /t REG_DWORD /d 1 /f
+Write-Output "Enabling powershell constrained language mode"
+try {
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell" /v EnableTranscripting /t REG_DWORD /d 1 /f
+}
+catch {
+    Write-Output "Failed to enable powershell constrained language mode"
+}
 
 #Disable SMB Compression
 #https://msrc.microsoft.com/update-guide/en-US/vulnerability/CVE-2020-0796
 Write-Output "Disabling SMB Compression for CVE 2020-0796"
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v DisableCompression /t REG_DWORD /d 1 /f
+try {
+    reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v DisableCompression /t REG_DWORD /d 1 /f
+}
+catch {
+    Write-Output "Failed to disable SMB Compression"
+}
 
 ## Appended more stuff cuz why not
 # Author: Christopher Goes
@@ -259,34 +360,73 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v Disa
 # The following comes from this gist: https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
 # Which was derived from here: https://github.com/Disassembler0/Win10-Initial-Setup-Script/
 # Raise UAC level
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Type DWord -Value 5
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Type DWord -Value 1
+Write-Host "Raising UAC level..."
+try {
+    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Type DWord -Value 0
+    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Type DWord -Value 5
+    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Type DWord -Value 1
+}
+catch {
+    Write-Host "Failed to raise UAC level."
+}
 
 # Disable sharing mapped drives between users
-Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLinkedConnections"
+Write-Host "Disabling sharing mapped drives between users..."
+try {
+    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLinkedConnections" -Type DWord -Value 0
+}
+catch {
+    Write-Host "Failed to disable sharing mapped drives between users."
+}
 
 # Stop and disable Home Groups services
 Write-Host "Stopping and disabling Home Groups services..."
-Stop-Service "HomeGroupListener"
-Set-Service "HomeGroupListener" -StartupType Disabled
-Stop-Service "HomeGroupProvider"
-Set-Service "HomeGroupProvider" -StartupType Disabled
+try {
+    Stop-Service "HomeGroupListener"
+    Set-Service "HomeGroupListener" -StartupType Disabled
+    Stop-Service "HomeGroupProvider"
+    Set-Service "HomeGroupProvider" -StartupType Disabled
+}
+catch {
+    Write-Host "Failed to stop and disable Home Groups services."
+}
 
 # Disable Remote Assistance
 Write-Host "Disabling Remote Assistance..."
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type DWord -Value 0
+try {
+    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type DWord -Value 0
+}
+catch {
+    Write-Host "Failed to disable Remote Assistance."
+}
 
 # Disable Autoplay
 Write-Host "Disabling Autoplay..."
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
+try {
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
+}
+catch {
+    Write-Host "Failed to disable Autoplay."
+}
 
 #Disable Sticky keys prompt
-Write-Host "Disabling Sticky keys prompt..." 
-Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+Write-Host "Disabling Sticky keys prompt..."
+try {
+    Set-ItemProperty -Path "HKCU:\Control Panel\Accessibility\StickyKeys" -Name "Flags" -Type String -Value "506"
+}
+catch {
+    Write-Host "Failed to disable Sticky keys prompt."
+}
 
 # Show hidden files
 Write-Host "Showing hidden files..."
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 1
+try {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 1
+}
+catch {
+    Write-Host "Failed to show hidden files."
+}
 
 #Verify secure channel is not already enforced
 Get-ADComputer -Filter {OperatingSystem -Like "*Windows Server*"} | ForEach-Object {
@@ -305,9 +445,3 @@ Get-WindowsUpdate -Install -AcceptAll -AutoReboot
 #disable sensitive permissions on everyone group on services
 #mitigate pass the hash attacks
 #mitigate printnightmare
-
-
-#enable enforcement of netlogon secure channel
-# Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -Name "RequireSignOrSeal" -Value 1 -Type DWord
-# Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -Name "RequireStrongKey" -Value 1 -Type DWord
-# Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters" -Name "FullSecureChannelProtection" -Value 1 -Type DWord
--- ansible/ssh_keys/README.md
+++
@@ -1,5 +0,0 @@
-# Generating ansible ssh keys in ssh_keys directory
-
-### Ansible Keys
-
-1. ```ssh-keygen -t ed25519 -f /path_to/ansible/ssh_keys/ansible```
